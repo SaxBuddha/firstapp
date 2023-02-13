720a5400f9b646c906f8b2be8fa76c4a855a9aae
SaxBuddha <saxbuddha@gmail.com>
2023-02-13 12:21:24 -0600

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+
+    return (
+
+        <container className="main">
+
+        <h1>Main goes here!</h1>
+        
+        </container>
+
+    )
+
+
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+//import pic from "../images/29305921348.jpg"
+
+export default function Navbar() {
+
+    
+    return (
+        
+        <nav>
+
+            <img src ="../images/Massysm_Quentin_—_The_Moneylender_and_his_Wife_—_1514.jpg" alt="logo" height="90px"/>
+            <h3>ReactFacts!</h3>
+            <h4>React-Project 1</h4>  
+
+        </nav>
+    )
+    
+
+
+}
